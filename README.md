a259f1d9a25be86d58d0e09bab498b027a0f09a1
Mayconcezar7 <maycon.cezar.souza7@gmail.com>
2025-03-30 15:17:51 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">Jokenpô</h1>
+<br>
+<h3> Esse foi um projeto que eu aprendi no <a href="https://www.devclub.com.br">Dev Club</a></h3>
+<br>
+<h2>🚀 Tecnologias Utilizadas</h2>
+<br>
+<h3>Este projeto foi desenvolvido com as seguintes tecnologias: </h3>
+
+HTML
+<br>
+CSS
+<br>
+Java Script
+
+
+<h3>Mobile:</h3>
+<img src="img/mobile.png">
+
+<h3>Desktop: </h3>
+<img src="img/Desktop.png">
---
+++ script.js
@@ -0,0 +1,54 @@
+
+let pontosH = 0
+let pontosM = 0
+
+const escolhaHumano = (humano) => {
+    vencedor(humano, escolhaMaquina())
+}
+
+
+const escolhaMaquina = () => {
+
+    const maquinaOp = ['papel', 'pedra', 'tesoura']
+
+    const sorteioMaquinaOp = Math.floor(Math.random() * 3)
+
+    return maquinaOp[sorteioMaquinaOp]
+
+}
+
+
+const vencedor = (humano, maquina) => {
+    const resultado = document.querySelector(".resultado")
+    const pontosHumano = document.querySelector("#humano-ponto")
+    const pontosMaquina = document.querySelector("#maquina-ponto")
+
+    const maquinaEscolha = document.querySelector(".maquina-escolha")
+
+
+
+
+    if (humano === maquina) {
+        
+        maquinaEscolha.innerHTML = `Ele escolheu: ${escolhaMaquina()}`
+        resultado.innerHTML = 'Deu empate'
+
+    }
+    else if ((humano === 'pedra' && maquina === 'tesoura') || (humano === 'tesoura' && maquina === 'papel') || (humano === 'papel' && maquina === 'pedra')) {
+
+        pontosH++
+
+        pontosHumano.innerHTML = pontosH
+        maquinaEscolha.innerHTML = `Ele escolheu: ${escolhaMaquina()}`
+        resultado.innerHTML = `Você ganhou!!!`
+
+    } else {
+
+        pontosM++
+
+        pontosMaquina.innerHTML = pontosM
+        maquinaEscolha.innerHTML = `Ele escolheu: ${escolhaMaquina()}`
+        resultado.innerHTML = `O Maycon ganhou!!!`
+    }
+
+}
